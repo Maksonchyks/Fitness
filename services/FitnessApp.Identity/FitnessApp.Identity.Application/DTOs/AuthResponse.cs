c4408734dced2947d1874979bb1d70be3ae7277d
Maksonchyks <musinmaksim28@gmail.com>
2026-01-13 16:39:52 +0200

    dtos for response

---
+++ services/FitnessApp.Identity/FitnessApp.Identity.Application/DTOs/AuthResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Identity.Application.DTOs
+{
+    public class AuthResponse
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+        public List<string> Roles { get; set; } = new();
+    }
+}
