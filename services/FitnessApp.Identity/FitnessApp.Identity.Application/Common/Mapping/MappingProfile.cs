e7787da06e93447b7bc29f0bccefcef6262e5f79
Maksonchyks <musinmaksim28@gmail.com>
2026-01-13 16:38:43 +0200

    add auto mapper

---
+++ services/FitnessApp.Identity/FitnessApp.Identity.Application/Common/Mapping/MappingProfile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using FitnessApp.Identity.Application.DTOs;
+using FitnessApp.Identity.Domain.Entities;
+
+namespace FitnessApp.Identity.Application.Common.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserResponse>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Value))
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.GetFullName()))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.GetAge()))
+                .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender.HasValue ? src.Gender.Value.ToString() : null))
+                .ForMember(dest => dest.FitnessGoal, opt => opt.MapFrom(src => src.FitnessGoal.HasValue ? src.FitnessGoal.Value.ToString() : null))
+                .ForMember(dest => dest.ProfileImageUrl, opt => opt.MapFrom(src => src.ProfileImageUrl))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+                .ForMember(dest => dest.EmailConfirmed, opt => opt.MapFrom(src => src.EmailConfirmed))
+                .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => src.UserRoles.Select(ur => ur.Role.Name).ToList()));
+
+            CreateMap<User, AuthResponse>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Value))
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.GetFullName()))
+                .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => 
+                    src.UserRoles.Select(ur => ur.Role.Name).ToList()));
+
+
+        }
+    }
+}
