899f9a4c9c97cb99400dd9eec4aa74fa2c9a3966
Maksonchyks <musinmaksim28@gmail.com>
2026-01-13 16:40:44 +0200

    register services at application layer

---
+++ services/FitnessApp.Identity/FitnessApp.Identity.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Identity.Application.Common.Behaviors;
+using FitnessApp.Identity.Application.Common.Mapping;
+using FitnessApp.Identity.Application.Interfaces;
+using FitnessApp.Identity.Application.Services;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitnessApp.Identity.Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            services.AddScoped<IEmailService, EmailService>();
+            services.AddScoped<IPasswordService, PasswordService>();
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
