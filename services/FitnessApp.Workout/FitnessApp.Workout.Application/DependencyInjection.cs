eedc79d0e01c8d47b04eac7f888831439bb3335c
Maksonchyks <musinmaksim28@gmail.com>
2026-02-14 13:17:48 +0200

    register dependencies

---
+++ services/FitnessApp.Workout/FitnessApp.Workout.Application/DependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Workout.Application.Common;
+using FitnessApp.Workout.Application.Interfaces.Generators;
+using FitnessApp.Workout.Application.Services.Generators;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitnessApp.Workout.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(cfg => {
+                cfg.RegisterServicesFromAssembly(assembly);
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            services.AddAutoMapper(assembly);
+
+            services.AddScoped<ITrainingProgramFactory, TrainingProgramFactory>();
+
+            services.AddScoped<ITrainingProgramGenerator, PowerliftingGenerator>();
+            services.AddScoped<ITrainingProgramGenerator, BodybuildingGenerator>();
+            services.AddScoped<ITrainingProgramGenerator, FitnessGenerator>();
+
+            return services;
+        }
+    }
+}
