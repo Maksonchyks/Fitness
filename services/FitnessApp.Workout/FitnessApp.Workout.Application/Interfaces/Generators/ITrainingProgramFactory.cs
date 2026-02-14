da01457d442a1d3d1bd3da28af4279b55e515f6f
Maksonchyks <musinmaksim28@gmail.com>
2026-02-14 13:14:19 +0200

    create interfaces for training factory generator

---
+++ services/FitnessApp.Workout/FitnessApp.Workout.Application/Interfaces/Generators/ITrainingProgramFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Workout.Domain.Enums;
+
+namespace FitnessApp.Workout.Application.Interfaces.Generators
+{
+    public interface ITrainingProgramFactory
+    {
+        ITrainingProgramGenerator GetGenerator(FitnessGoal goal);
+    }
+}
---
+++ services/FitnessApp.Workout/FitnessApp.Workout.Application/Interfaces/Generators/ITrainingProgramGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Workout.Domain.Entities;
+using FitnessApp.Workout.Domain.Enums;
+using FitnessApp.Workout.Domain.ValueObjects;
+
+namespace FitnessApp.Workout.Application.Interfaces.Generators
+{
+    public interface ITrainingProgramGenerator
+    {
+        FitnessGoal SupportedGoal { get; }
+        TrainingProgram Generate(ProgramProfile profile);
+    }
+}
