c8c66624122451a8e53cc86653fbb17beb25a44e
Maksonchyks <musinmaksim28@gmail.com>
2026-02-14 13:16:53 +0200

    add mapping

---
+++ services/FitnessApp.Workout/FitnessApp.Workout.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using FitnessApp.Workout.Application.DTOs;
+using FitnessApp.Workout.Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FitnessApp.Workout.Application.Mappings
+{
+    public class TrainingProgramMappingProfile : Profile
+    {
+        public TrainingProgramMappingProfile()
+        {
+            CreateMap<TrainingProgram, TrainingProgramResponse>();
+
+            CreateMap<TrainingDay, TrainingDayDto>();
+
+            CreateMap<ExerciseSet, ExerciseSetDto>();
+        }
+    }
+}
