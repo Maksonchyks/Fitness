51b12ec659a5214a6949eb3c55ff2c5da2f33de3
Maksonchyks <musinmaksim28@gmail.com>
2026-02-14 13:19:29 +0200

    dbcontext

---
+++ services/FitnessApp.Workout/FitnessApp.Workout.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Workout.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessApp.Workout.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<TrainingProgram> TrainingPrograms => Set<TrainingProgram>();
+        public DbSet<WorkoutSession> WorkoutSessions => Set<WorkoutSession>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
