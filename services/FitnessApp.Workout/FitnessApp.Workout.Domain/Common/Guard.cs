1834e92107cccfe0e68c9ffae14acc0394aebecb
Maksonchyks <musinmaksim28@gmail.com>
2026-02-14 12:58:36 +0200

    create guard for fields validation

---
+++ services/FitnessApp.Workout/FitnessApp.Workout.Domain/Common/Guard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Workout.Domain.Exceptions;
+
+namespace FitnessApp.Workout.Domain.Common
+{
+    public static class Guard
+    {
+        public static void AgainstNullOrEmpty(string value, string parameterName)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new DomainException($"{parameterName} cannot be null or empty");
+        }
+
+        public static void AgainstNegativeValue(float value)
+        {
+            if (value < 0)
+                throw new DomainException($"{value} cannot be negative");
+        }
+    }
+}
