60168883700638e5634313666fe9dbba1b4ba552
Maksonchyks <musinmaksim28@gmail.com>
2026-02-14 13:07:44 +0200

    add value objects for power metrics and user profile

---
+++ services/FitnessApp.Workout/FitnessApp.Workout.Domain/ValueObjects/PowerMetrics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Workout.Domain.Common;
+
+namespace FitnessApp.Workout.Domain.ValueObjects
+{
+    public sealed class PowerMetrics : ValueObject
+    {
+        public float SquatWeight { get; init; }
+        public float BenchPressWeight { get; init; }
+        public float DeadliftWeight { get; init; }
+
+        public PowerMetrics(float squatWeight, float benchPressWeight, float deadliftWeight)
+        {
+            Guard.AgainstNegativeValue(squatWeight);
+            Guard.AgainstNegativeValue(benchPressWeight);
+            Guard.AgainstNegativeValue(deadliftWeight);
+
+            SquatWeight = squatWeight;
+            BenchPressWeight = benchPressWeight;
+            DeadliftWeight = deadliftWeight;
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return SquatWeight;
+            yield return BenchPressWeight;
+            yield return DeadliftWeight;
+        }
+    }
+}
---
+++ services/FitnessApp.Workout/FitnessApp.Workout.Domain/ValueObjects/ProgramProfile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Workout.Domain.Enums;
+using FitnessApp.Workout.Domain.Exceptions;
+
+namespace FitnessApp.Workout.Domain.ValueObjects
+{
+    public class ProgramProfile : ValueObject
+    {
+        public Guid UserId { get; }
+        public FitnessGoal Goal { get; }
+        public Intensity Intensity { get; }
+        public PowerMetrics? PowerMetrics { get; }
+
+        public ProgramProfile(
+            Guid userId,
+            FitnessGoal goal,
+            Intensity intensity,
+            PowerMetrics? powerMetrics)
+        {
+            if (userId == Guid.Empty)
+                throw new DomainException("UserId cannot be empty");
+
+            if ((goal == FitnessGoal.Powerlifting || goal == FitnessGoal.Bodybuilding)
+                && powerMetrics is null)
+                throw new DomainException("Power metrics required for strength goals");
+
+            UserId = userId;
+            Goal = goal;
+            Intensity = intensity;
+            PowerMetrics = powerMetrics;
+        }
+
+        protected override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return UserId;
+            yield return Goal;
+            yield return Intensity;
+            yield return PowerMetrics;
+        }
+    }
+}
