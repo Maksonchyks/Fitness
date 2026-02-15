af3a8d1eeca49bc941fdcd94a25209a712c73c87
Maksonchyks <musinmaksim28@gmail.com>
2026-02-15 16:44:33 +0200

    refactor exercise set from entities into value object

--- services/FitnessApp.Workout/FitnessApp.Workout.Domain/Events/WorkoutCompleted.cs
+++ services/FitnessApp.Workout/FitnessApp.Workout.Domain/Events/WorkoutCompleted.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FitnessApp.Workout.Domain.Entities;
 using FitnessApp.Workout.Domain.Interfaces;
+using FitnessApp.Workout.Domain.ValueObjects;
 
 namespace FitnessApp.Workout.Domain.Events
 {
