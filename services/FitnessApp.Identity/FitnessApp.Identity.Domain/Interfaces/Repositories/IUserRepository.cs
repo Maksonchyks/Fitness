a014ef0276656952fea4d32cc25be1fd504cc481
Maksonchyks <musinmaksim28@gmail.com>
2026-04-30 12:37:03 +0300

    add method for remove all roles

--- services/FitnessApp.Identity/FitnessApp.Identity.Domain/Interfaces/Repositories/IUserRepository.cs
+++ services/FitnessApp.Identity/FitnessApp.Identity.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +11,14 @@ namespace FitnessApp.Identity.Domain.Interfaces.Repositories
     public interface IUserRepository
     {
         Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<User>> GetAllAsync(CancellationToken cancellationToken = default);
         Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
         Task<User?> GetByUsernameAsync(string username, CancellationToken cancellationToken = default);
         Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default);
         Task<bool> ExistsByUsernameAsync(string username, CancellationToken cancellationToken = default);
         Task AddAsync(User user, CancellationToken cancellationToken = default);
         void Update(User user);
+        Task ClearUserRolesAsync(Guid userId, CancellationToken cancellationToken = default);
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
